a9189c3fb4801721cebbaeada1622d1d373dc148
Layer3 <me@layer3.ie>
2018-05-17 17:53:26 +0100

    Update pbx-brander.sh

--- pbx-brander.sh
+++ pbx-brander.sh
@@ -8,6 +8,7 @@ if ! grep 'intellicom-logo-white.png' /opt/pbxware/pw/home/sitemanager/admin/css
   background-size: 75% !important;
 }
 EOF
+fi
 
 if ! grep 'intellicom-logo-white.png' /opt/pbxware/pw/home/sitemanager/admin/css/main_flex.css &>/dev/null; then
   cat <<EOF >> /opt/pbxware/pw/home/sitemanager/admin/css/main_flex.css
@@ -17,6 +18,7 @@ if ! grep 'intellicom-logo-white.png' /opt/pbxware/pw/home/sitemanager/admin/css
   background-size: 100% !important;
 }
 EOF
+fi
 
 mkdir -p mkdir /opt/pbxware/pw/home/sitemanager/admin/css/img
 curl -L 'https://github.com/layer3git/pbx-brander/raw/master/intellicom-logo-white.png' >/opt/pbxware/pw/home/sitemanager/admin/css/img/intellicom-logo-white.png
